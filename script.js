780a833dde6d9b331bdfe45ae2d1ee0492cec684
Rahul Roy <roymentor4@gamil.com>
2026-02-02 15:38:20 +0530

    Updated website files

---
+++ script.js
@@ -0,0 +1,23 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     const text = document.getElementById("text");
+
+//     let mouseX = 0, mouseY = 0;
+//     let currentX = 0, currentY = 0;
+
+//     document.addEventListener("mousemove", (e) => {
+//         mouseX = e.clientX;
+//         mouseY = e.clientY;
+//     });
+
+//     function animate() {
+//         currentX += (mouseX - currentX) * 0.1;
+//         currentY += (mouseY - currentY) * 0.1;
+
+//         text.style.left = currentX + "px";
+//         text.style.top = currentY + "px";
+
+//         requestAnimationFrame(animate);
+//     }
+
+//     animate();
+// });
